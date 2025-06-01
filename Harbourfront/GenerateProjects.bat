0ea131d602bc3b380bc710496cf4a38d6dcb719b
Jacob Pratley <pratleyjacob@gmail.com>
2025-06-01 13:37:15 -0400

    Updated CMake and gitignore settings

--- Harbourfront/GenerateProjects.bat
+++ Harbourfront/GenerateProjects.bat
@@ -1,3 +1,4 @@
+@echo off
 set OUTPUT_DIR=Generated
 set INSTALL_DIR=%CD%\install
 set GENERATOR="Visual Studio 17 2022"
@@ -6,3 +7,17 @@ set CONFIG_TYPES="Debug;Release"
 if not exist %OUTPUT_DIR% mkdir %OUTPUT_DIR%
 cd %OUTPUT_DIR%
 cmake -G %GENERATOR% -DCMAKE_CONFIGURATION_TYPES=%CONFIG_TYPES% -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% ..\
+
+echo Cleaning up CMake junk.. If you want something done right you've got to do it yourself I guess
+cd ..
+
+for /D %%D in (*) do (
+	if /I not "%%D"=="%OUTPUT_DIR%" (
+		if exist "%%D\cmake_install.cmake" (
+			del "%%D\cmake_install.cmake"
+		)
+		if exist "%%D\CMakeFiles" (
+			rmdir /s /q "%%D\CMakeFiles"
+		)
+	)
+)
