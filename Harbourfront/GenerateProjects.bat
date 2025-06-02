709d1571b02034bdc609d02cc0c75fc06715ebe7
Jacob Pratley <pratleyjacob@gmail.com>
2025-06-01 21:48:32 -0400

    Added Retail build configuration

--- Harbourfront/GenerateProjects.bat
+++ Harbourfront/GenerateProjects.bat
@@ -1,12 +1,10 @@
-
 set OUTPUT_DIR=Generated
 set INSTALL_DIR=%CD%\install
 set GENERATOR="Visual Studio 17 2022"
-set CONFIG_TYPES="Debug;Release"
 
 if not exist %OUTPUT_DIR% mkdir %OUTPUT_DIR%
 cd %OUTPUT_DIR%
-cmake -G %GENERATOR% -DCMAKE_CONFIGURATION_TYPES=%CONFIG_TYPES% -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% ..\
+cmake -G %GENERATOR% -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% ..\
 
 echo Cleaning up CMake junk.. If you want something done right you've got to do it yourself I guess
 cd ..
--- Harbourfront/HFGame/HF_Main.cpp
+++ Harbourfront/HFGame/HF_Main.cpp
@@ -1,6 +1,10 @@
 #include "PCH/HFGame_PCH.h"
 
+#if YAKU_RETAIL
+int WinMain()
+#else
 int main()
+#endif // YAKU_RETAIL
 {
 	printf("Hello, Harbourfront!");
 	return 0;
